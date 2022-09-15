26dff86505288e2dab2caabaf2ec033372db8f5e
keruha <kirpan1994@mail.ru>
2022-09-15 10:34:05 +0300

    initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import App from "./App";
+export default function Layout({ initState }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `window.initState=${JSON.stringify(initState)}`,
+          }}
+        />
+        <script defer src="/app.js" />
+        <script defer src="/vendor.js" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div id="root">
+          <App {...initState} />
+        </div>
+      </body>
+    </html>
+  );
+}
